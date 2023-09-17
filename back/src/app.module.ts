8232fa7b17efd88c48dc68cb0095b5e82b6e816a
gonza <alvarezgonzalo0022@gmail.com>
2023-09-17 20:30:28 -0300

    First project structure

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule } from './clients/clients.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { EmployeesModule } from './employees/employees.module';
+import { BranchesModule } from './branches/branches.module';
 
 @Module({
-  imports: [],
+  imports: [ClientsModule, AccountsModule, TransactionsModule, EmployeesModule, BranchesModule],
   controllers: [AppController],
   providers: [AppService],
 })
